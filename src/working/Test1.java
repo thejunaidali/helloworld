fd8062ab9eabd6d23a428b1009ed660899a771cd
junaid <junaidali8214@gmail.com>
2022-11-24 12:18:34 +0530

    first commit

---
+++ src/working/Test1.java
@@ -0,0 +1,18 @@
+package working;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int [] array1 = {20, 30, 40, 50, 60, 70};
+		
+		 System.out.println("Total numbers are " + array1.length);
+		 System.out.println("The first number is " + array1[0]);
+		 System.out.println("The second number is " + array1[1]);
+		 System.out.println("The last number is " + array1[array1.length -1]);
+		 
+		System.out.println("i am junaid ali");
+	}
+
+}
